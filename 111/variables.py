25ad2ff45a9b6d482cdae2d815b8751b9055ca87
Кампутэр <2275946@gmail.com>
2024-11-18 21:16:41 +0300

    Initial commit

---
+++ 111/variables.py
@@ -0,0 +1,5 @@
+quantity_homework = 12
+quantity_hours = 1.5
+course_name = 'Python'
+task_time = quantity_hours/quantity_homework
+print('Курс '+ str(course_name) +', всего задач :'+ str(quantity_homework) +', затрачено часов:'+ str(quantity_hours) +', среднее время выполнения '+ str(task_time)+' часа')
