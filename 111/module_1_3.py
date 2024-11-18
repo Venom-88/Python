18cf78e53b10e346ef3686ca979c9708f49fedeb
Кампутэр <2275946@gmail.com>
2024-11-18 22:00:30 +0300

    Initial commit

--- 111/module_1_3.py
+++
@@ -1,8 +0,0 @@
-name = 'Dmitry'
-print(name)
-age = 36
-print(age)
-new_age = age+1
-print(new_age)
-is_student = True
-print(is_student)
--- 111/module_1_4.py
+++
@@ -1,8 +0,0 @@
-my_string = input('Введите текст: ')
-print(my_string)
-print(len(my_string))
-print(my_string.upper())
-print(my_string.lower())
-print(my_string.replace(' ', ''))
-print(my_string[0])
-print(my_string[-1:])
