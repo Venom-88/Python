987ee2c6a84bfaaa93c0d1d5d90134dd51ccd956
Кампутэр <2275946@gmail.com>
2024-11-29 18:45:12 +0300

    Initial commit

--- module_2/module_2_2.py
+++ module_2/module_2_2.py
@@ -4,7 +4,7 @@ third = int(input())
 
 if first == second and first == third:
     print(3)
-elif first == second or first == third:
+elif first == second or first == third or second == third:
     print(2)
 else:
     print(0)
