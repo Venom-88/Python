3c5f0e73010c7bc600861642e0ebdcdda2199389
Кампутэр <2275946@gmail.com>
2024-12-08 17:24:49 +0300

    Initial commit

--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -2,14 +2,15 @@ def get_multiplied_digits(number):
     str_number = str(number)
     if len(str_number) > 1:
         first = int(str_number[0])
-        return first * get_multiplied_digits(int(str_number[1:]))
+        if first == 0:
+            return get_multiplied_digits(int(str_number[1:]))
+        else:
+            return first * get_multiplied_digits(int(str_number[1:]))
     else:
-        return int(str_number)
+        last_digit = int(str_number)
+        return 1 if last_digit == 0 else last_digit
 
 result = get_multiplied_digits(40203)
-
 print(result)
-
 result2 = get_multiplied_digits(402030)
-
 print(result2)
